a45b46a16b857ab46d793dd4dba7aebd12786644
Christian Baehr <cbaehr@aiddata.wm.edu>
2020-02-14 17:02:14 -0500

    Update merge_data.R

--- merge_data.R
+++ merge_data.R
@@ -89,22 +89,30 @@ wave4 <- SpatialPolygonsDataFrame(Sr=wave4$geometry, data = data.frame(wave4), m
 
 roads <- st_read(paste0(box_loc, "/inputData/inpii_roads.geojson"), stringsAsFactors = F)
 roads <- SpatialLinesDataFrame(sl = as(roads$geometry, 'Spatial'), data = data.frame(roads), match.ID = F)
-roads <- over(wave3, roads, returnList = T)
+wave3roads <- over(wave3, roads, returnList = T)
 
 extract.dates <- function(x) {
   y <- as.Date(x[["date"]], tryFormats="%m/%d/%y", optional=T)
   return(as.character(y[!is.na(y)]))
 }
 
-roads <- lapply(roads, FUN = extract.dates)
+wave3roads <- lapply(wave3roads, FUN = extract.dates)
 
 # wave3 <- as.data.frame(wave3, stringsAsFactors=F)
 
-wave3$roads <- roads
+wave3$roads <- wave3roads
 # wave3$geometry <- as(wave3$geometry, "Spatial")
 # wave3 <- SpatialPolygonsDataFrame(Sr=wave3$geometry, data = wave3, match.ID = F)
 wave3$roads <- sapply(wave3$roads, length)
 wave3$treatment <- ifelse(wave3$roads>0, 1, 0)
+
+
+wave4roads <- over(wave4, roads, returnList = T)
+wave4roads <- lapply(wave4roads, FUN = extract.dates)
+
+wave4$roads <- wave4roads
+wave4$roads <- sapply(wave4$roads, length)
+wave4$treatment <- ifelse(wave4$roads>0, 1, 0)
 
 ###
 
@@ -260,6 +268,115 @@ test3$q706 <- ifelse(test3$q706=="I strongly agree", 2,
                             ifelse(test3$q706=="I disagree", -1,
                                    ifelse(test3$q706=="I strongly disagree", -2, NA))))
 
+#################################################
+
+test4$age <- as.numeric(test4$q1001)
+test4$male <- ifelse(test4$q1002=="Male", 1, 0)
+
+test4$education <- ifelse(test4$q1003=="MA and above", 7,
+                          ifelse(test4$q1003=="BA", 6,
+                                 ifelse(test4$q1003=="Mid-level diploma/professional or technical", 5,
+                                        ifelse(test4$q1003=="Secondary", 4,
+                                               ifelse(test4$q1003=="Preparatory/Basic", 3,
+                                                      ifelse(test4$q1003=="Elementary", 2,
+                                                             ifelse(test4$q1003=="Illiterate/No formal education", 1, NA)))))))
+
+test4$married <- ifelse(test4$q1010=="Married", 1, 0)
+
+test4$muslim <- ifelse(test4$q1012=="Muslim", 1, 0)
+
+test4$christian <- ifelse(test4$q1012=="Christian", 1, 0)
+
+### No explicit income measure in test4
+
+test4$urban <- ifelse(test4$v05=="city", 1, 0)
+test4$rural <- ifelse(test4$v05 %in% c("Village/Town", "Village/ Town"), 1, 0)
+test4$refugee_camp <- ifelse(test4$v05=="Refugee Camp", 1, 0)
+
+test4$employed <- ifelse(test4$q1004=="Yes", 1, 0)
+
+test4$full_time <- ifelse(test4$q1006=="Full time (30 hours or more a week)", 1, 0)
+
+test4$public_employee <- ifelse(test4$q1006a=="Public", 1,
+                                ifelse(test4$q1006a=="", NA, 0))
+
+test4$retired <- ifelse(test4$q1005=="Retired", 1,
+                        ifelse(test4$q1005=="Decline to answer (Do not read)", NA, 0))
+
+test4$housewife <- ifelse(test4$q1005=="Housewife", 1,
+                          ifelse(test4$q1005=="Decline to answer (Do not read)", NA, 0))
+
+test4$student <- ifelse(test4$q1005=="Student", 1,
+                        ifelse(test4$q1005=="Decline to answer (Do not read)", NA, 0))
+
+test4$student <- ifelse(test4$q1005=="Student", 1,
+                        ifelse(test4$q1005=="Decline to answer (Do not read)", NA, 0))
+
+test4$unemployed <- ifelse(test4$q1005=="Unemployed", 1,
+                           ifelse(test4$q1005=="Decline to answer (Do not read)", NA, 0))
+
+### No home ownership question in Wave IV
+
+test4$own_a_computer <- ifelse(test4$q1011a=="Yes", 1, 0)
+test4$own_a_car <- ifelse(test4$q1011b=="Yes", 1, 0)
+
+## future economic relations btwn palestine/US
+test4$q7001 <- ifelse(test4$q7001=="Become stronger than they were in previous years", 1,
+                      ifelse(test4$q7001=="Remain the same as they were in previous years", 0,
+                             ifelse(test4$q7001=="Become weaker than they were in previous years", -1, NA)))
+
+## future economic relations btwn palestine/Saudi
+test4$q7002 <- ifelse(test4$q7002=="Become stronger than they were in previous years", 1,
+                      ifelse(test4$q7002=="Remain the same as they were in previous years", 0,
+                             ifelse(test4$q7002=="Become weaker than they were in previous years", -1, NA)))
+
+
+## influence of US on development of democracy in your country. Not asked in Wave III
+test4$q7011 <- ifelse(test4$q7011=="Very positive", 2,
+                      ifelse(test4$q7011=="Somewhat positive", 1,
+                             ifelse(test4$q7011=="Neither positive nor negative", 0,
+                                    ifelse(test4$q7011=="Somewhat negative", -1,
+                                           ifelse(test4$q7011=="Very negative", -2, NA)))))
+
+## do you think (ordinary) americans are generally good people? Slight variation of Wave III
+test4$q707 <- ifelse(test4$q707=="Agree", 1,
+                     ifelse(test4$q707=="Disagree", 0, NA))
+
+## American and Western culture have positive aspects? Not asked in Wave III
+test4$q708 <- ifelse(test4$q708=="Strongly agree", 2,
+                     ifelse(test4$q708=="Agree", 1,
+                            ifelse(test4$q708=="Disagree", -1,
+                                   ifelse(test4$q708=="Strongly disagree", -2, NA))))
+
+## Do you support the two-state Arab-Israeli solution?
+test4$q709a <- ifelse(test4$q709a=="Support", 1,
+                      ifelse(test4$q709a=="Oppose", 0, NA))
+
+
+## Is western infuence an obstacle to reform in your country?
+test4$q7114 <- ifelse(test4$q7114=="Agree to a large extent", 2,
+                      ifelse(test4$q7114=="Agree to some extent", 1,
+                             ifelse(test4$q7114=="Don’t agree", -1,
+                                    ifelse(test4$q7114=="Don’t agree at all", -2, NA))))
+
+## What country poses the greatest threat to stability in your country? (US dummy)
+test4$q714us <- ifelse(test4$q714=="The United States", 1, 
+                       ifelse(test4$q714 %in% c("Decline to answer (Do not read)", "Don't know (Do not read)"), NA, 0))
+
+## What country poses the greatest threat to stability in your country? (SA dummy)
+test4$q714sa <- ifelse(test4$q714=="Saudi Arabia", 1, 
+                       ifelse(test4$q714 %in% c("Decline to answer (Do not read)", "Don't know (Do not read)"), NA, 0))
+
+## What country poses the greatest threat to stability in your country? (ISR dummy)
+test4$q714is <- ifelse(test4$q714=="Israel", 1, 
+                       ifelse(test4$q714 %in% c("Decline to answer (Do not read)", "Don't know (Do not read)"), NA, 0))
+
+## to what degree you would describe yourself as feeling angry toward the US?
+test4$q8341 <- ifelse(test4$q8341=="Very angry", 4,
+                      ifelse(test4$q8341=="Somewhat angry", 3,
+                             ifelse(test4$q8341=="Not very angry", 2,
+                                    ifelse(test4$q8341=="Not angry at all", 1, NA))))
+
 
 #################################################
 
@@ -270,7 +387,6 @@ for(i in unique(test3$qid)) {
   trend <- lm(dmsp ~ c(1:6))
   test3$dmsp_pretrend[test3$qid==i] <- trend$coefficients[2]
 }
-
 
 sum(test4$v05aENG %in% test3$v05aENG)
 sum(test3$v05aENG %in% test4$v05aENG)
@@ -285,7 +401,7 @@ test3$v05aENG[test3$v05aENG=="Old-City-Nablus"] <- "Nablus City"
 unique(test3$v05aENG[!test3$v05aENG %in% test4$v05aENG])
 test3 <- test3[which(test3$v05aENG %in% test4$v05aENG),]
 
-unique(test4$q2[!test4$v05aENG %in% test3$v05aENG])
+unique(test4$v05aENG[!test4$v05aENG %in% test3$v05aENG])
 test4 <- test4[test4$v05aENG %in% test3$v05aENG,]
 
 out_names <- c("age",
@@ -309,13 +425,13 @@ out_labels <- c("Age",
                 "Owns a car (dummy)")
 
 stargazer(test3@data[, out_names], type = "latex", title = "Wave III Demographics (Full Sample)",
-          covariate.labels = out_labels, out = "/Users/christianbaehr/Desktop/demographics_full.tex")
+          covariate.labels = out_labels, out = "/Users/christianbaehr/Desktop/demographics_full_wave3.tex")
 
 stargazer(test3@data[test3$treatment==1, out_names], type = "latex", title = "Wave III Demographics (Treatment Only)",
-          covariate.labels = out_labels, out = "/Users/christianbaehr/Desktop/demographics_treatment.tex")
+          covariate.labels = out_labels, out = "/Users/christianbaehr/Desktop/demographics_treatment_wave3.tex")
 
 stargazer(test3@data[test3$treatment==0, out_names], type = "latex", title = "Wave III Demographics (Control Only)",
-          covariate.labels = out_labels, out = "/Users/christianbaehr/Desktop/demographics_control.tex")
+          covariate.labels = out_labels, out = "/Users/christianbaehr/Desktop/demographics_control_wave3.tex")
 
 ###
 
@@ -340,15 +456,15 @@ out_labels <- c("Treatment (dummy)",
                 "Refugee Camp (dummy)")
 
 stargazer(test3@data[, out_names], type = "latex", title = "Wave III Covariates (Full Sample)",
-          covariate.labels = out_labels, out = "/Users/christianbaehr/Desktop/covariates_full.tex", 
+          covariate.labels = out_labels, out = "/Users/christianbaehr/Desktop/covariates_full_wave3.tex", 
           digits.extra = 0)
 
 stargazer(test3@data[test3$treatment==1, out_names], type = "latex", title = "Wave III Covariates (Treatment Only)",
-          covariate.labels = out_labels, out = "/Users/christianbaehr/Desktop/covariates_treatment.tex",
+          covariate.labels = out_labels, out = "/Users/christianbaehr/Desktop/covariates_treatment_wave3.tex",
           digits.extra = 0)
 
 stargazer(test3@data[test3$treatment==0, out_names], type = "latex", title = "Wave III Covariates (Control Only)",
-          covariate.labels = out_labels, out = "/Users/christianbaehr/Desktop/covariates_control.tex",
+          covariate.labels = out_labels, out = "/Users/christianbaehr/Desktop/covariates_control_wave3.tex",
           digits.extra = 0)
 
 ###
@@ -360,6 +476,7 @@ out_names <- c("q7001",
                "q700a2",
                "q700a6",
                "q706",
+               "q707",
                "q701c")
 
 out_labels <- c("Do you prefer economic relations btwn Pal./US improve",
@@ -369,18 +486,19 @@ out_labels <- c("Do you prefer economic relations btwn Pal./US improve",
                 "Do you prefer security relations btwn Pal./Saudi improve",
                 "Do you prefer security relations btwn Pal./Israel improve",
                 "Do you agree: US interference justifies arms against the US?",
+                "Americans are generally good people",
                 "Should foreign aid to your country increase?")
 
 stargazer(test3@data[, out_names], type = "latex", title = "Wave III Survey Questions (Full Sample)",
-          covariate.labels = out_labels, out = "/Users/christianbaehr/Desktop/survey_full.tex", 
+          covariate.labels = out_labels, out = "/Users/christianbaehr/Desktop/survey_full_wave3.tex", 
           digits.extra = 0)
 
 stargazer(test3@data[test3$treatment==1, out_names], type = "latex", title = "Wave III Survey Questions (Treatment Only)",
-          covariate.labels = out_labels, out = "/Users/christianbaehr/Desktop/survey_treatment.tex",
+          covariate.labels = out_labels, out = "/Users/christianbaehr/Desktop/survey_treatment_wave3.tex",
           digits.extra = 0)
 
 stargazer(test3@data[test3$treatment==0, out_names], type = "latex", title = "Wave III Survey Questions (Control Only)",
-          covariate.labels = out_labels, out = "/Users/christianbaehr/Desktop/survey_control.tex",
+          covariate.labels = out_labels, out = "/Users/christianbaehr/Desktop/survey_control_wave3.tex",
           digits.extra = 0)
 
 ###
@@ -388,9 +506,94 @@ stargazer(test3@data[test3$treatment==0, out_names], type = "latex", title = "Wa
 writeOGR(obj = test3[names(test3)!="geometry"], dsn = "/Users/christianbaehr/Desktop/wave3.geojson",
          layer = "qid", driver = "GeoJSON")
 
+#################################################
+
+out_names <- c("age",
+               "male",
+               "education",
+               "married",
+               "muslim",
+               "christian",
+               "employed",
+               "own_a_car")
+
+out_labels <- c("Age",
+                "Male (dummy)",
+                "Level of Educ., 0=None, 7=MA+",
+                "Married (dummy)",
+                "Muslim (dummy)",
+                "Christian (dummy)",
+                "Employed (dummy)",
+                "Owns a car (dummy)")
+
+stargazer(test4@data[, out_names], type = "latex", title = "Wave IV Demographics (Full Sample)",
+          covariate.labels = out_labels, out = "/Users/christianbaehr/Desktop/demographics_full_wave4.tex")
+
+stargazer(test4@data[test4$treatment==1, out_names], type = "latex", title = "Wave IV Demographics (Treatment Only)",
+          covariate.labels = out_labels, out = "/Users/christianbaehr/Desktop/demographics_treatment_wave4.tex")
+
+stargazer(test4@data[test4$treatment==0, out_names], type = "latex", title = "Wave IV Demographics (Control Only)",
+          covariate.labels = out_labels, out = "/Users/christianbaehr/Desktop/demographics_control_wave4.tex")
+
+
+###
+
+out_names <- c("q7001",
+               "q7002",
+               "q7011",
+               "q707",
+               "q708",
+               "q709a",
+               "q7114",
+               "q714us",
+               "q714sa",
+               "q714is",
+               "q8341")
+
+out_labels <- c("Do you prefer economic relations btwn Pal./US improve",
+                "Do you prefer economic relations btwn Pal./Saudi improve",
+                "How does US influence democracy development in Pal.",
+                "*Ordinary* Americans are generally good people",
+                "American and Western culture has positive aspects",
+                "Do you support the two-state Arab-Israeli solution", 
+                "Is western influence an obstacle to reform in Pal.?",
+                "What country poses greatest threat to your country (US Dummy)",
+                "What country poses greatest threat to your country (Saudi Dummy)",
+                "What country poses greatest threat to your country (ISR Dummy)",
+                "To what degree do you feel angry towards the US?")
+
+stargazer(test4@data[, out_names], type = "latex", title = "Wave IV Survey Questions (Full Sample)",
+          covariate.labels = out_labels, out = "/Users/christianbaehr/Desktop/survey_full_wave4.tex", 
+          digits.extra = 0)
+
+stargazer(test4@data[test4$treatment==1, out_names], type = "latex", title = "Wave IV Survey Questions (Treatment Only)",
+          covariate.labels = out_labels, out = "/Users/christianbaehr/Desktop/survey_treatment_wave4.tex",
+          digits.extra = 0)
+
+stargazer(test4@data[test4$treatment==0, out_names], type = "latex", title = "Wave IV Survey Questions (Control Only)",
+          covariate.labels = out_labels, out = "/Users/christianbaehr/Desktop/survey_control_wave4.tex",
+          digits.extra = 0)
+
+
+###
+
+writeOGR(obj = test4[names(test4)!="geometry"], dsn = "/Users/christianbaehr/Desktop/wave4.geojson",
+         layer = "qid", driver = "GeoJSON")
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+#################################################
 #################################################
 
 
